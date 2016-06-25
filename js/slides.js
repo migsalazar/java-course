bf91e5d8d9be3178fe1f8c2fb4230d24fef2e128
Mig Salazar <mig.salazars@gmail.com>
2016-06-24 21:31:45 -0500

    add external plugin

--- js/slides.js
+++ js/slides.js
@@ -1,5 +1,27 @@
 $(function(){
+  var
+    revealConfig = {
+      controls: true,
+      progress: true,
+      slideNumber: true,
+      keyboard: true,
+      overview: true,
+      center: true,
+      touch: true,
+      loop: false,
+      mouseWheel: true,
+      hideAddressBar: true,
+      previewLinks: true,
+      transitionSpeed: 'default'
+    },
+    plugins = {
+      dependencies: [
+          { src: 'revealjs/plugin/highlight/highlight.js', async: true },
+          { src: 'revealjs/plugin/zoom-js/zoom.js', async: true },
+          { src: 'revealjs/plugin/external/external.js', async: true, condition: function() { return !!document.querySelector( '[data-external]' ); } }
+      ]
+    };
 
-      Reveal.initialize();
-
+  Reveal.initialize(revealConfig);
+  Reveal.configure(plugins);
 });
