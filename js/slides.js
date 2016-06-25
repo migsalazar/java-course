fcc4fc5adf21446da54eb6cd67fd0ec5c2e1d178
Mig Salazar <mig.salazars@gmail.com>
2016-06-24 21:59:21 -0500

    external doesnt work

--- js/slides.js
+++ js/slides.js
@@ -17,11 +17,19 @@ $(function(){
     plugins = {
       dependencies: [
           { src: 'revealjs/plugin/highlight/highlight.js', async: true },
-          { src: 'revealjs/plugin/zoom-js/zoom.js', async: true },
-          { src: 'revealjs/plugin/external/external.js', async: true, condition: function() { return !!document.querySelector( '[data-external]' ); } }
+          { src: 'revealjs/plugin/zoom-js/zoom.js', async: true }
       ]
-    };
+    },
+    injectSections = function() {
+      $('.slides > section').each(function(index, section){
+        var $section = $(section),
+            file = $section.data('html');
+        $.get(file, function(rawHtml) {
+          $section.html(rawHtml);
+        });
+      });
+    }();//execute immediately
 
-  Reveal.initialize(revealConfig);
+  Reveal.initialize(revealConfig)
   Reveal.configure(plugins);
 });
